63e329e2221ec12b9197d62388e7ec582bf265f0
FLR <fanchleroux@tutanota.com>
2020-08-03 11:13:18 -0400

    Starting building nodeJS server

--- global.js
+++ global.js
@@ -1,6 +1,5 @@
 //Show portfolio sections on nav
 function showPortfolio(){
-    console.log("portfolio")
     let portfolio = document.getElementById("portfolio");
     portfolio.style.display = "block"
   }
